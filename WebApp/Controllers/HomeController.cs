ba67098b009e7e57e031bf5d97875746e5e233d5
rifki ahmad sururi <rifkisururi27@gmail.com>
2021-02-24 22:51:28 +0700

    Add project files.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private databaseEntities db = new databaseEntities();
+
+        public ActionResult Index(string pencarian = "")
+        {
+            return View( WebApp.Models.barangModel.getData(pencarian));            
+        }
+
+        public ActionResult pembalikKata(string kata = "")
+        {
+            int jumlahKarakter;
+            string output;
+            output = "";
+            jumlahKarakter = kata.Length - 1;
+            for (int i = jumlahKarakter; i >= 0; i--) {
+                output = System.String.Concat( output, kata.Substring(i, 1) );
+
+            }
+
+            
+
+            ViewBag.kataTerbalik = output;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/barangModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class barangModel
+    {
+
+        private static databaseEntities db = new databaseEntities();
+        public DbSet<BARANG> b { get; set; }
+        public string product_id { get; set; }
+        public string produkName { get; set; }
+        public int? price{ get; set; }
+
+        public static IEnumerable<WebApp.Models.barangModel> getData(string pencarian) {
+
+            var query = from b in db.BARANGs
+                        select new barangModel
+                        {
+                            product_id = b.product_id,
+                            produkName = b.product_name,
+                            price = b.price
+                        };
+            if (pencarian == "")
+            {
+                return query.ToList();
+            }
+            else {
+                return query.ToList().Where(b => b.product_id.Contains(pencarian) || b.produkName.Contains(pencarian));
+                //return null;
+            }
+            
+
+        }
+    }
+}
